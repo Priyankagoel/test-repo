cd9a75faa9f75d7ac2c166e0dd2525f2631773dd
priyanka <priyanka.goel1997@gmail.com>
2019-05-14 15:35:57 +0530

    validation complete

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const { validationResult } = require("express-validator/check");
+
+
 exports.getPosts = (req, res, next) => {
     return res.status(200).json({
         posts: [{ title:'First Post', content:'This is my first post'}]
@@ -5,10 +8,22 @@ exports.getPosts = (req, res, next) => {
 };  
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+                message: 'Validation error',
+                errors: errors.array()
+            });
+    }
     const title = req.body.title;
     const content = req.body.content;
-   return res.status(201).json({
+    return res.status(201).json({
         message: 'Post created Sucessfully',
-        post: {id: new Date().toISOString(), title: title, content: content}
+        post: {
+            id: new Date().toISOString(), 
+            title: title, 
+            content: content,
+            createdAt: new Date()
+        }
     });
 };
